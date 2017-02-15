86cb2ab3db7ff2c25eeb1c55b834b0d71462ce80
Pedro Batista <pedosb@gmail.com>
2017-02-15 12:31:05 -0300

    Metabot DETRAN

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
-english_bot = ChatBot("English Bot")
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english")
+meta_bot = ChatBot("English Bot")
+meta_bot.set_trainer(ChatterBotCorpusTrainer)
+meta_bot.train("data.detran",
+               "chatterbot.corpus.portuguese.greetings")
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/get/<string:query>")
-def get_raw_response(query):
-    return str(english_bot.get_response(query))
-
+@app.route("/get", methods=['POST', 'GET'])
+def get_raw_response():
+    return_data = dict()
+    return_data['text'] = str(meta_bot.get_response(request.json['text']))
+    response = app.response_class(
+        response=json.dumps(return_data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=7000)
